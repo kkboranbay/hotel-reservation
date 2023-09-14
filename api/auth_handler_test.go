45b386e78089bd10377ac3a55b95061f03aac899
kkboranbay <kkboranbay@gmail.com>
2023-09-14 17:56:48 +0600

    Creating tests for the authentication handler

---
+++ api/auth_handler_test.go
@@ -0,0 +1,118 @@
+package api
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/kkboranbay/hotel-reservation/db"
+	"github.com/kkboranbay/hotel-reservation/types"
+)
+
+func insertTestUser(t *testing.T, userStore db.UserStore) *types.User {
+	user, err := types.NewUserFromParams(types.CreateUserParams{
+		FirstName: "Leo",
+		LastName:  "Ken",
+		Email:     "leoken@gmail.com",
+		Password:  "password",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = userStore.InsertUser(context.TODO(), user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return user
+}
+
+func TestAuthSuccess(t *testing.T) {
+	tdb := setup(t)
+	defer tdb.teardown(t)
+
+	insertedUser := insertTestUser(t, tdb.UserStore)
+
+	app := fiber.New()
+	authHandler := NewAuthHandler(tdb.UserStore)
+	app.Post("/auth", authHandler.HandleAuthenticate)
+
+	params := AuthParams{
+		Email:    "leoken@gmail.com",
+		Password: "password",
+	}
+	b, _ := json.Marshal(params)
+	req := httptest.NewRequest("POST", "/auth", bytes.NewReader(b))
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("expected 200 but got %d", resp.StatusCode)
+	}
+
+	var authResp AuthResponse
+	if err := json.NewDecoder(resp.Body).Decode(&authResp); err != nil {
+		t.Fatal(err)
+	}
+
+	if authResp.Token == "" {
+		t.Fatalf("expected the JWT token to be presented in the auth response")
+	}
+
+	// Set the encrypted password to an empty string, because we don't return
+	// that in any JSON response.
+	insertedUser.EncryptedPassword = ""
+	if !reflect.DeepEqual(insertedUser, authResp.User) {
+		t.Fatalf("expected the user to be the inserted user")
+	}
+}
+
+func TestAuthWithWrongPassword(t *testing.T) {
+	tdb := setup(t)
+	defer tdb.teardown(t)
+
+	insertTestUser(t, tdb.UserStore)
+
+	app := fiber.New()
+	authHandler := NewAuthHandler(tdb.UserStore)
+	app.Post("/auth", authHandler.HandleAuthenticate)
+
+	params := AuthParams{
+		Email:    "leoken@gmail.com",
+		Password: "passwordNotCorrect",
+	}
+	b, _ := json.Marshal(params)
+	req := httptest.NewRequest("POST", "/auth", bytes.NewReader(b))
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Fatalf("expected 400 but got %d", resp.StatusCode)
+	}
+
+	var genericResp genericResponse
+	if err := json.NewDecoder(resp.Body).Decode(&genericResp); err != nil {
+		t.Fatal(err)
+	}
+
+	if genericResp.Type != "error" {
+		t.Fatalf("expected type to be error but got %s", genericResp.Type)
+	}
+
+	if genericResp.Msg != "invalid credentials" {
+		t.Fatalf("expected msg to be <invalid credentials> but got %s", genericResp.Msg)
+	}
+}
